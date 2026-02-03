18a47a6e263c82eb8be53635c0834c448fb708b6
Taha Murad Zaman <142559741+Tamz-0@users.noreply.github.com>
2026-02-03 11:52:44 +0530

    Time: 0 ms (100%), Space: 55.6 MB (70.97%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isTrionic = function(nums) {
+    const n=nums.length;
+    if(n<3)return false;
+    let i=0;
+    while(i+1<n&&nums[i]<nums[i+1])i++;
+    if(i==0||i==n-1)return false;
+    let mid=i;
+    while(i+1<n&&nums[i]>nums[i+1])i++;
+    if(i==mid||i==n-1)return false;
+    while(i+1<n&&nums[i]<nums[i+1])i++;
+    return i==n-1;
+
+    
+    
+};
