50fc9b1c0048ad115be5268870cfa772490b8bbb
Taha Murad Zaman <142559741+Tamz-0@users.noreply.github.com>
2026-05-01 19:52:31 +0530

    Time: 23 ms (5.97%), Space: 11.7 MB (100%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def romanToInt(self, s):
+        romanValues = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+
+        for i in range(len(s) - 1, -1, -1):
+            currValue = romanValues[s[i]]
+
+            if i < len(s) - 1 and currValue < romanValues[s[i + 1]]:
+                result -= currValue
+            else:
+                result += currValue
+
+        return result
+        
