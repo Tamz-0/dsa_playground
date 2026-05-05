1a711b000d5e6f89a7c02b1b942c7eaa17894a51
Taha Murad Zaman <142559741+Tamz-0@users.noreply.github.com>
2026-05-06 00:48:44 +0530

    Time: 0 ms (100%), Space: 13 MB (77.09%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int max_length=0;
+        int i=0;
+        while(i<sentences.size())
+        {
+            int length=0;
+            for(int j=0;j<sentences[i].size();j++)
+            {
+                if(sentences[i][j]==' ')
+                   length++;
+            }
+            length+=1; // to include to last word of sentences[i]. U can also initialise length=1 to avoid this increment at end.
+            max_length = max(max_length,length);
+            i++;
+        }
+        return max_length;
+    }
+};
